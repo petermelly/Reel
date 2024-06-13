c765e30443df8fbf7741791756dd7381ff26cc51
Peter <kotoekonay390@gmail.com>
2024-06-13 12:44:41 +0630

    add video more

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import './App.css';
-import { Box } from '@mui/material';
-import ButtonAppBar from './conponent/ButtonAppBar';
-import Bodybox from './conponent/nextbody';
+import React from "react";
+import "./App.css";
+import { Box } from "@mui/material";
+import ButtonAppBar from "./conponent/ButtonAppBar";
+import Bodybox from "./conponent/nextbody";
 
-let App=()=> {
+let App = () => {
   return (
     <Box className="App">
-      <ButtonAppBar/>
+      <ButtonAppBar />
       <header className="App-header">
-      <div className='app_video'>
-        <Bodybox imageURL='https://storage.googleapis.com/web-dev-assets/video-and-source-tags/chrome.webm' title='chrome.webm'/>
-        <Bodybox imageURL='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4' title='ForBiggerEscapes'/>
-        <Bodybox imageURL='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4' title='BigBuckBunny'/>
-        <Bodybox imageURL='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4' title='ForBiggerBlazes'/>
-        <Bodybox imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4" title='ElephantsDream'/>
-        <Bodybox imageURL='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4' title='WhatCarCanYouGetForAGrand'/>
-        <Bodybox imageURL='http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4' title='SubaruOutbackOnStreetAndDirt'/>
-      </div>
+        <div className="app_video">
+          <Bodybox imageURL="/video1.mp4" title="sport1" />
+          <Bodybox
+            imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+            title="ForBiggerEscapes"
+          />
+          <Bodybox
+            imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+            title="BigBuckBunny"
+          />
+          <Bodybox
+            imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+            title="ForBiggerBlazes"
+          />
+          <Bodybox imageURL="/video2.mp4" title="sport2" />
+          <Bodybox
+            imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+            title="ElephantsDream"
+          />
+          <Bodybox
+            imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"
+            title="WhatCarCanYouGetForAGrand"
+          />
+          <Bodybox imageURL="/video3.mp4" title="sport3" />
+          <Bodybox
+            imageURL="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
+            title="SubaruOutbackOnStreetAndDirt"
+          />
+        </div>
       </header>
     </Box>
   );
-}
+};
 
 export default App;
